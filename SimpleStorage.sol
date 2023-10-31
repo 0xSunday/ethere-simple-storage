04a9a3402e241af6833091dffd826ee9f08afa91
Sunil Reddy <sunday7637@gmail.com>
2023-10-31 13:44:21 +0530

    final

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,25 +2,29 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract SimpleStorage {
-    
-    uint256 public favoriteNumber;
 
-    mapping(string => uint256) public nameToFavoriteNumber;
-    mapping(uint256 => string) public favoriteNumberToName;
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
     struct People {
-        uint256 fn;
+        uint256 favoriteNumber;
         string name;
     }
+    // uint256[] public anArray;
     People[] public people;
 
-    function store(uint256 _fn) public virtual {
-        favoriteNumber = _fn;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
     }
 
-    function addPerson(uint256 _fn, string memory _nm) public {
-        people.push(People(_fn, _nm));
-        nameToFavoriteNumber[_nm] = _fn;
-        favoriteNumberToName[_fn] = _nm;
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
--- deploy.js
+++ deploy.js
@@ -1,34 +1,36 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
+const ethers = require("ethers")
+// const solc = require("solc")
+const fs = require("fs-extra")
+require("dotenv").config()
+
 async function main() {
-  let provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
-
-  const wallet = new ethers.Wallet(
-    "0x91ee00225ac6c137ad02d95e8f86a3556df646a5992e96f8c5f8f2b71f73587c",
-    provider
-  );
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
-  const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
-
-  const deployContract = new ethers.ContractFactory(abi, bin, wallet);
-  console.log("contract is deploying, please wait you moron.....");
-  const contract = await deployContract.deploy();
-  await contract.deploymentTransaction.wait(1);
-
-  const currentFavoriteNumber = await contract.retrieve();
-  console.log(
-    `current favorite number is : ${currentFavoriteNumber.toString()}`
-  );
-  const trasactionResponse = await contract.store("10");
-  const trasactionReceipt = await trasactionResponse.wait(1);
-  const newFavoriteNumber = await contract.retrieve();
-  console.log(
-    `new and updated favorite number is : ${newFavoriteNumber.toString()}`
-  );
+   
+    let provider = new ethers.JsonRpcProvider(process.env.RPC_URL)
+    let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    )
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying, please wait...")
+    const contract = await contractFactory.deploy()
+    // const deploymentReceipt = await contract.deployTransaction.wait(1)
+    console.log(`Contract deployed to ${contract.address}`)
+  
+    let currentFavoriteNumber = await contract.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+    console.log("Updating favorite number...")
+    let transactionResponse = await contract.store(7)
+    // let transactionReceipt = await transactionResponse.wait()
+    currentFavoriteNumber = await contract.retrieve()
+    console.log(`New Favorite Number: ${currentFavoriteNumber}`)
 }
+
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
